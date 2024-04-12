5f9a4529533b971753838997ebd74b86839330d5
Sheng Feng <114194642+sfeng1@users.noreply.github.com>
2024-04-12 11:53:44 -0700

    add auth back

--- src/public/scripts/mail_auth.js
+++ src/public/scripts/mail_auth.js
@@ -10,6 +10,12 @@ const nodemailer = require("nodemailer");
 let transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
+      type: 'OAuth2',
+      user: 'softwareprogrammingquiz2024@gmail.com',
+      pass: 'CS467spq2024',
+      clientId: '684146792452-7m59l3iasil6jbqsg3eluat3schqnfhi.apps.googleusercontent.com',
+      clientSecret: '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',
+      refreshToken: '1//044zPIDIXlzMlCgYIARAAGAQSNwF-L9IrjIyoD0uJ5QxNUfOSNI3AQfgrABuditC92f0lplKJ-BGcmVcmkX5ghvZ0tYOJEUvH7LU'
     }
 });
 
