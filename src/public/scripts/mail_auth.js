017594d772f13fbb17c55c112bdb3325beb15c0a
Sheng Feng <114194642+sfeng1@users.noreply.github.com>
2024-04-12 11:52:11 -0700

    remove secret

--- src/public/scripts/mail_auth.js
+++ src/public/scripts/mail_auth.js
@@ -10,12 +10,6 @@ const nodemailer = require("nodemailer");
 let transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-      type: 'OAuth2',
-      user: 'softwareprogrammingquiz2024@gmail.com',
-      pass: 'CS467spq2024',
-      clientId: '684146792452-7m59l3iasil6jbqsg3eluat3schqnfhi.apps.googleusercontent.com',
-      clientSecret: ,
-      refreshToken: 
     }
 });
 
